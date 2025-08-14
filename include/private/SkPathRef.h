ff5dff720c67413daeef349ee4311a590fa5da3b
Armin Sander <armin@replicator.org>
2025-08-14 14:54:20 +0200

    rust-skia: Remove declarations with missing implementations.

--- include/private/SkPathRef.h
+++ include/private/SkPathRef.h
@@ -265,7 +265,8 @@ public:
         return fBounds;
     }
 
-    SkRRect getRRect() const;
+    // rust-skia: Implementation is missing.
+    // SkRRect getRRect() const;
 
     /**
      * Transforms a path ref by a matrix, allocating a new one only if necessary.
